03c31472d8a8378595a2b57fef5314b7767ba7a3
Alex Silva <alexsantossilva44@gmail.com>
2026-06-17 22:17:30 +0100

    Alterando o arquivo linguagem.js

--- linguagem.js
+++ linguagem.js
@@ -1,5 +1,6 @@
 function linguagemDosGatosECachorros(animal) {
   if (animal == 'cachorro') return 'au au';
   else if (animal == 'gato') return 'miau';
-  else return 'Informe gato ou cachorro';
+  else if (animal == 'vaca') return 'muuu';
+  else return 'Informe gato, cachorro ou vaca';
 }
